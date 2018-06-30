a48dcad3424e4ef447154f7148adaf115a89a373
Stuart O'Donnell <stuartodonnell@gmail.com>
2018-06-30 11:17:03 +0100

    Add server setup, dependencies & basic seeds

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const port = 3001;
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended:true}));
+
+const MongoClient = require("mongodb").MongoClient;
+MongoClient.connect("mongodb://localhost:27017", (err, client)=>{if (err) next(err);
+const db = client.db("elementsdb");
+console.log("Connected to DB");
+
+app.listen(port, ()=>{
+  console.log("app listening on port", port);
+})
+
+})
