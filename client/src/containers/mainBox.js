f65be760b63e8d3f543c521b27f7de073d6a7972
Stuart O'Donnell <stuartodonnell@gmail.com>
2018-06-30 12:24:25 +0100

    seeds file changed and then reset

---
+++ client/src/containers/mainBox.js
@@ -0,0 +1,20 @@
+require React from ('react');
+
+class MainBox extends React.Component{
+
+constructor(props){
+  super(props);
+    this.state = {
+      element: []
+    }
+  }
+
+  render(){
+    return (
+      
+    )
+  }
+
+}
+
+export default MainBox;
