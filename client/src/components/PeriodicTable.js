c92feb41456c213c7b49f52eb75bf6586fbe3bba
AndrewLowrie <andrew2lowrie@googlemail.com>
2018-07-01 11:38:54 +0100

    Add basic css responsive structuring to table layout

--- client/src/components/PeriodicTable.js
+++ client/src/components/PeriodicTable.js
@@ -19,20 +19,19 @@ class PeriodicTable extends React.Component{
     }
 
     return (
-      <div>
-      <h1>Physics Yay!</h1>
-        <div className="row">{grid.slice(0, 18)} </div>
-        <div className="row">{grid.slice(18, 36)}</div>
-        <div className="row">{grid.slice(36, 54)}</div>
-        <div className="row">{grid.slice(54, 72)}</div>
-        <div className="row">{grid.slice(72, 90)}</div>
-        <div className="row">{grid.slice(90, 108)}</div>
-        <div className="row">{grid.slice(108, 126)}</div>
-        <div className="row">{grid.slice(126, 144)}</div>
-        <div className="row">{grid.slice(144, 162)}</div>
-        <div className="row">{grid.slice(162, 180)}</div>
-
-  </div>
+      <div className="period-table">
+        <h1>Physics Yay!</h1>
+          <div className="row">{grid.slice(0, 18)} </div>
+          <div className="row">{grid.slice(18, 36)}</div>
+          <div className="row">{grid.slice(36, 54)}</div>
+          <div className="row">{grid.slice(54, 72)}</div>
+          <div className="row">{grid.slice(72, 90)}</div>
+          <div className="row">{grid.slice(90, 108)}</div>
+          <div className="row">{grid.slice(108, 126)}</div>
+          <div className="row">{grid.slice(126, 144)}</div>
+          <div className="row">{grid.slice(144, 162)}</div>
+          <div className="row">{grid.slice(162, 180)}</div>
+        </div>
     )
   }
 
