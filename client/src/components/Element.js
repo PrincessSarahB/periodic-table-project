3140a965ff9e76e2826fdbb674092f6a2168e61a
AndrewLowrie <andrew2lowrie@googlemail.com>
2018-07-01 19:54:26 +0100

    Add element category to button class names and style button colours

--- client/src/components/Element.js
+++ client/src/components/Element.js
@@ -2,14 +2,13 @@ import React from 'react';
 import "./element.css";
 class Element extends React.Component {
   render(){
-
     let elementInfo;
 
     if (this.props.elementDetails !== undefined) {
       const element = this.props.elementDetails
       elementInfo = (
         <div id={[element.xpos, element.ypos]} className="element" value={element}>
-          <button>
+          <button className={element.category}>
           <p> <strong> {element.number} </strong> </p>
           <p> <strong> {element.symbol} </strong> </p>
           <p> <strong> {element.name} </strong> </p>
