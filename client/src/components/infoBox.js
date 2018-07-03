8f276cde09ce6f2c382e6e96d22b94c7dffde2f7
Stuart O'Donnell <stuartodonnell@gmail.com>
2018-07-03 13:44:27 +0100

    adds infobox styling for scroll and justification

--- client/src/components/infoBox.js
+++ client/src/components/infoBox.js
@@ -5,27 +5,28 @@ class InfoBox extends React.Component {
     const element = (this.props.displayElement)
 
     const elementInfo = (
-      <div className="info-box">
-          <button className="close" onClick={this.props.closeInfoBox}>X</button>
+      <div>
+        <button className="close" onClick={this.props.closeInfoBox}>X</button>
         <h1>Element Info</h1>
-        <div className="video">
-        <iframe width="320" height="215" src="https://www.youtube.com/embed/rz4Dd1I_fX0" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
-      </div>
+
+
         <div className="element-info">
-        <ul className="t">
+          <iframe width="640" height="340" src="https://www.youtube.com/embed/rz4Dd1I_fX0" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+
+          <ul className="t">
             <li className="li-name">Name: {element.name} </li>
             <li className="li-symbol">Symbol: {element.symbol} </li>
             <li className="li-appearance">Appearance: {element.appearance} </li>
             <li className="li-summary">Summary: {element.summary} </li>
-            <li className="li-atomic-mass">Atomic Mass: {element.atomic_mass} </li>
+            {/* <li className="li-atomic-mass">Atomic Mass: {element.atomic_mass} </li> */}
             <li className="li-category">Category: {element.category} </li>
             <li className="li-discoverer">Discovered By: {element.discovered_by} </li>
-            <li className="li-namer">Named By: {element.named_by} </li>
+            {/* <li className="li-namer">Named By: {element.named_by} </li> */}
             <li className="li-number">Number: {element.number} </li>
             <li className="li-phase">Phase: {element.phase} </li>
             <li className="li-link"> <a href={element.source}> Read more </a> </li>
-        </ul>
-      </div>
+          </ul>
+        </div>
 
       </div>
 
